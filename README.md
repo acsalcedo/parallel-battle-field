03d23a99bc115d5ba2095f82dd31edb93886f6e2
Andrea Salcedo <andreacsalcedo@gmail.com>
2015-11-29 12:47:16 -0430

    Se agregaron dos casos de prueba.

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # parallel-battle-field
 Proyecto 3 para Sistemas de Operación 3 - Sept-Dic 2015
+
+### Para compilar:
+
+        mpicc battle-field.c -o battle
+
+### Para correr las pruebas:
+
+1. Prueba 1:
+        mpirun --disable-hostname-propagation -n 4 -machinefile maquinas.txt ./battle ./tests/test
+2. Prueba 2:
+        mpirun --disable-hostname-propagation -n 9 -machinefile maquinas.txt ./battle ./tests/test2
+3. Prueba 3:
+        mpirun --disable-hostname-propagation -n 22 -machinefile maquinas.txt ./battle ./tests/test3
+
+
+### Resultados:
+
+##### Prueba 1
+
+| Tipo de Blanco                       | Resultado |
+| ------------------------------------ | :-------: |
+| Military Targets totally destroyed   |     2     |
+| Military Targets partially destroyed |     0     |
+| Military Targets not affected        |     0     |
+| Civilian Targets totally destroyed   |     0     |
+| Civilian Targets partially destroyed |     2     |
+| Civilian Targets not affected        |     0     |
+
+
+##### Prueba 2
+
+| Tipo de Blanco                       | Resultado |
+| ------------------------------------ | :-------: |
+| Military Targets totally destroyed   |     1     |
+| Military Targets partially destroyed |     4     |
+| Military Targets not affected        |     0     |
+| Civilian Targets totally destroyed   |     1     |
+| Civilian Targets partially destroyed |     2     |
+| Civilian Targets not affected        |     1     |
+
+
+##### Prueba 3
+
+| Tipo de Blanco                       | Resultado |
+| ------------------------------------ | :-------: |
+| Military Targets totally destroyed   |     4     |
+| Military Targets partially destroyed |     8     |
+| Military Targets not affected        |     1     |
+| Civilian Targets totally destroyed   |     4     |
+| Civilian Targets partially destroyed |     3     |
+| Civilian Targets not affected        |     2     |
--- src/battle-field.c
+++ src/battle-field.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <mpi.h>
 
+//Estructura de un ataque.
 struct Attack {
     int coordx;
     int coordy;
@@ -9,6 +10,7 @@ struct Attack {
     int power;
 };
 
+//Estructura de un blanco.
 struct Target {
     int coordx;
     int coordy;
@@ -16,16 +18,11 @@ struct Target {
     int oldStrength;
 };
 
+//Estructura del resultado de un blanco.
 struct Resultado {
     int strength;
     int oldStrength;
 };
-
-
-/*
-    Para correr MPI: $ mpirun -np 4 parallel-battle-field test
-    Para compilar con MPI : $ mpicc battle-field.c -o parallel-battle-field -std=c99 -g
-*/
 
 int main (int argc, char **argv) {
 
@@ -45,47 +42,41 @@ int main (int argc, char **argv) {
     struct Target *blancos;
     struct Resultado resultado;
 
-    // inicializamos MPI y le pasamos los argumentos del programa
+    //Se inicializa MPI y se le pasa los argumentos del programa
     MPI_Comm_size(MPI_COMM_WORLD, &numeroProcesos);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
 
     fscanf(file, "%i", &size);      //Lee el tamano del campo.
     fscanf(file, "%i", &targets);   //Lee la cantidad de blancos.
 
-    //int inicio = id*(targets / numeroProcesos);
-    //int fin = inicio + (targets / numeroProcesos) - 1;
-
-    // Lee todas las lineas relevantes a los blancos, guarda el blanco
-    // que le pertenece al proceso.
-
     blancos = (struct Target *) malloc(sizeof(struct Target));
-    if (blancos == NULL)
-    {
+
+    if (blancos == NULL) {
         fprintf(stderr, "No hay memoria\n");
         exit(1);
     }
 
+    /* Lee todas las lineas relevantes a los blancos, guarda el blanco
+     * que le pertenece al proceso. */
     for (i = 0; i < targets; i++) {
         if (i == id) {
             fscanf(file, "%i %i %i", &(blancos->coordx), &(blancos->coordy), &(blancos->strength));
             blancos->oldStrength = blancos->strength;
-        }
-        else
+        } else
             fscanf(file, "%*i %*i %*i");
     }
 
     fscanf(file, "%i", &numAttacks);    //Lee el numero de ataques.
 
-    // Arreglo con los ataques.
+    //Arreglo con los ataques.
     attacks = malloc(numAttacks * sizeof(struct Attack));
-    if (attacks == NULL)
-    {
+    if (attacks == NULL) {
         fprintf(stderr, "No hay memoria\n");
         free(blancos);
         exit(1);
     }
 
-    // Almacena los valores de los ataques en el arreglo de ataques.
+    //Almacena los valores de los ataques en el arreglo de ataques.
     for (i = 0; i < numAttacks; i++) {
         struct Attack singleAttack;
         fscanf(file, "%i %i %i %i", &singleAttack.coordx, &singleAttack.coordy,
@@ -93,26 +84,13 @@ int main (int argc, char **argv) {
         attacks[i] = singleAttack;
     }
 
-    // Imprime la informacion.
-    /*printf("Size: %i, Targets: %i \n", size, targets);
-    printf("Target coordinates: (%i,%i) Strength: %i\n", coordx, coordy, strength);
-    printf("Attacks: %i\n", numAttacks);
-
-    for (i = 0; i < numAttacks; i++) {
-        struct Attack singleAttack = attacks[i];
-        printf("%i %i %i %i\n", singleAttack.coordx, singleAttack.coordy,
-                                singleAttack.radius, singleAttack.power);
-    }*/
-
-
-    // Simular el ataque para un proceso (id 0)
-    printf("Target coordinates: (%i,%i) Strength: %i\n",
-            blancos->coordx, blancos->coordy, blancos->strength);
     int radioX_Sup = 0, radioY_Sup = 0;
     int radioX_Inf = 0, radioY_Inf = 0;
     int destruidos_CT = 0, parcial_destruidos_CT = 0, intactos_CT = 0;
     int destruidos_MT = 0, parcial_destruidos_MT = 0, intactos_MT = 0;
 
+    /* Hace los calculos de los ataques en los blancos. Cada proceso calcula
+     * la fuerza del blanco despues del ataque. */
     for (i = 0; i < numAttacks; i++) {
         struct Attack singleAttack = attacks[i];
         radioX_Sup = singleAttack.coordx + singleAttack.radius;
@@ -123,68 +101,55 @@ int main (int argc, char **argv) {
         if ((radioX_Inf <= blancos->coordx && blancos->coordx <= radioX_Sup)
             && (radioY_Inf <= blancos->coordy && blancos->coordy <= radioY_Sup)) {
 
-            //printf("B%d -> Power: %d\n", i, singleAttack.power);
             // Si el blanco es militar
-            if (blancos->oldStrength < 0)
-            {
+            if (blancos->oldStrength < 0) {
                 blancos->strength += singleAttack.power;
+
                 // Si el blanco es destruido asigna 0
                 if (blancos->strength >= 0)
                     blancos->strength = 0;
-            }
-            else
-            {
+            } else {
                 blancos->strength -= singleAttack.power;
+
                 // Si el blanco es destruido asigna 0
                 if (blancos->strength <= 0)
                     blancos->strength = 0;
             }
         }
-
     }
-
     resultado.oldStrength = blancos->oldStrength;
     resultado.strength = blancos->strength;
 
-    if (id == 0)
-    {
+    //El proceso 0 recibe los resultados de los otros procesos y los imprime.
+    if (id == 0) {
         struct Resultado respuesta[targets];
         respuesta[0] = resultado;
-        for (i = 1; i < numeroProcesos; ++i)
-        {
-            // Recibo la respuesta
-            MPI_Recv(&respuesta[i], 1,
-                            MPI_2INT,
-                                MPI_ANY_SOURCE,
-                                    MPI_ANY_TAG,
-                                        MPI_COMM_WORLD,
-                                            MPI_STATUS_IGNORE);
-        }
 
-        for (i = 0; i < targets; ++i)
-        {
+        for (i = 1; i < numeroProcesos; ++i) {
+            //Recibe la respuesta
+            MPI_Recv(&respuesta[i],2,MPI_INT,MPI_ANY_SOURCE,MPI_ANY_TAG,
+                     MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        }
+        for (i = 0; i < targets; ++i) {
             // Si el blanco es CT
-            if (respuesta[i].oldStrength > 0)
-            {
+            if (respuesta[i].oldStrength > 0) {
+
                 if (respuesta[i].oldStrength > respuesta[i].strength &&
                     respuesta[i].strength > 0)
                     parcial_destruidos_CT += 1;
-                else
-                {
+                else {
                     if (respuesta[i].strength == 0)
                         destruidos_CT += 1;
                     else
                     intactos_CT += 1;
                 }
+            } else {
 
-            }
-            else
-            {
                 if (respuesta[i].oldStrength < respuesta[i].strength &&
                     respuesta[i].strength < 0)
                     parcial_destruidos_MT += 1;
-                else
-                {
+
+                else {
                     if (respuesta[i].strength == 0)
                         destruidos_MT += 1;
                     else
@@ -192,20 +157,20 @@ int main (int argc, char **argv) {
                 }
             }
         }
-
-        // Imprimir resultados
+        //Imprime los resultados
         printf("Military Targets totally destroyed: %d\n", destruidos_MT);
         printf("Military Targets partially destroyed: %d\n", parcial_destruidos_MT);
         printf("Military Targets not affected: %d\n", intactos_MT);
         printf("Civilian Targets totally destroyed: %d\n", destruidos_CT);
         printf("Civilian Targets partially destroyed: %d\n", parcial_destruidos_CT);
         printf("Civilian Targets not affected: %d\n", intactos_CT);
+
+    //Manda el resultado al proceso 0.
     } else
-        MPI_Send(&resultado,1,MPI_2INT,0,0,MPI_COMM_WORLD);
+        MPI_Send(&resultado,2,MPI_INT,0,0,MPI_COMM_WORLD);
 
     free(blancos);
     free(attacks);
     MPI_Finalize();
-    //printf("Resultado:\n");
-    //printf("T%d -> Strength: %i\n", blancos->id, blancos->strength);
+    return 0;
 }
